fix edge_gating computation

models.py
@@ -101,7 +101,7 @@ class RGCNLayer(nn.Module):
                 b = self.gate_bias[edges.data['rel_type']]
                 print(edges.src[feature_name].unsqueeze(1).shape, w.shape, b.shape)
                 print(torch.bmm(edges.src[feature_name].unsqueeze(1), w).shape)
-                edge_score = torch.sigmoid(torch.bmm(edges.src[feature_name].unsqueeze(1), w).squeeze() + b)
+                edge_score = torch.sigmoid(torch.bmm(edges.src[feature_name].unsqueeze(1), w).squeeze(1) + b)
                 print(msg.shape, edge_score.shape)
                 msg = edge_score * msg
             return {'msg': msg}