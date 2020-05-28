4b8d2c8b1e99eaa69573e961da3a5d4fd20327cb
Victor <victormilewski@gmail.com>
2020-05-28 07:55:56 +0200

    fixed passing wrong mask

--- eval.py
+++ eval.py
@@ -119,7 +119,7 @@ def beam_evaluate(data_name, checkpoint_file, data_folder, beam_size, outdir, gr
             cgat_mask[:, :cgat_mask_out.size(1)] = cgat_mask_out  # copy over mask from io attention
             cgat_obj[~cgat_mask & om] = of[~cgat_mask & om]  # fill the no in_degree nodes with the original state
             # we pass the object mask. We used the cgat_mask only to determine which io's where filled and which not.
-            graph_weighted_enc = decoder.cascade1_attention(cgat_obj, h1, mask=cgat_mask)
+            graph_weighted_enc = decoder.cascade1_attention(cgat_obj, h1, mask=om)
             img_weighted_enc = decoder.cascade2_attention(image_features, torch.cat([h1, graph_weighted_enc], dim=1))
             h2, c2 = decoder.language_model(torch.cat([graph_weighted_enc, img_weighted_enc, h1], dim=1), (h2, c2))
             scores = decoder.fc(h2)  # (s, vocab_size)
