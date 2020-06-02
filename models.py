fa32bb4f4782bf68e3b7c4d3dbaf9441d43b8d34
Victor <victormilewski@gmail.com>
2020-06-02 18:58:21 +0200

    debugging gat

--- models.py
+++ models.py
@@ -135,10 +135,10 @@ class Decoder(nn.Module):
         graphs = graphs.to(device)
 
         graph_features = self.gat(graphs, graphs.ndata['x'])
-        print(graphs.ndata['x'].shape,graph_features.shape)
         graph_features = torch.split(graph_features, graphs.batch_num_nodes)
         graph_features = pad_sequence(graph_features, batch_first=True).squeeze(2)
         graph_mask = graph_features.sum(dim=-1) != 0
+        print(graph_mask)
 
         graph_features_mean = graph_features.sum(dim=1) / graph_mask.sum(dim=1, keepdim=True)
         graph_features_mean = graph_features_mean.to(device)
